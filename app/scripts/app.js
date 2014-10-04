c9decb41fa28ebe9b52b9f4b495fc58221ec6339
ltischuk <ltischuk@accesso.com>
2014-10-04 14:27:06 -0400

    project scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name allegroIoApp
+ * @description
+ * # allegroIoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('allegroIoApp',
+    ['ui.router']);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name allegroIoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the allegroIoApp
+ */
+angular.module('allegroIoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
